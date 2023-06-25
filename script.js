ce211034ed4bb1c077c6148cd729e5d7f266b131
sojib600 <sojibdas400@gmail.com>
2023-06-25 21:12:09 +0600

    almost done

--- script.js
+++ script.js
@@ -1,113 +1,141 @@
-$(document).ready(function() {
-    $('#sort-button').click(function() {
-        var playlistUrl = $('#playlist-input').val();
-        if (playlistUrl) {
-            var playlistId = extractPlaylistId(playlistUrl);
-            $.ajax({
-                url: 'https://www.googleapis.com/youtube/v3/playlistItems',
-                dataType: 'json',
-                data: {
-                    part: 'snippet',
-                    maxResults: 50,
-                    playlistId: playlistId,
-                    key: 'AIzaSyDjieCRye5T3oMUGna3uE9fqZ6XQMemtXU'
-                },
-                success: function(response) {
-                    var playlistData = [];
-                    var videoIds = []; // Store video IDs to fetch video length
-                    
-                    $.each(response.items, function(index, item) {
-                        var videoData = item.snippet;
-                        playlistData.push({
-                            title: '<a href="https://www.youtube.com/watch?v=' + videoData.resourceId.videoId + '" target="_blank">' + videoData.title + '</a>',
-                            uploadDate: videoData.publishedAt,
-                            videoId: videoData.resourceId.videoId,
-                            likes: 0, // Retrieve likes count using YouTube API
-                            comments: 0 // Retrieve comments count using YouTube API
-                        });
-                        videoIds.push(videoData.resourceId.videoId); // Add video ID to fetch video length
-                    });
-                    
-                    // Fetch video length using YouTube API
-                    $.ajax({
-                        url: 'https://www.googleapis.com/youtube/v3/videos',
-                        dataType: 'json',
-                        data: {
-                            part: 'contentDetails',
-                            id: videoIds.join(','),
-                            key: 'AIzaSyDjieCRye5T3oMUGna3uE9fqZ6XQMemtXU'
-                        },
-                        success: function(response) {
-                            $.each(response.items, function(index, item) {
-                                var videoId = item.id;
-                                var duration = item.contentDetails.duration;
-                                var videoLength = parseISO8601Duration(duration);
-                                playlistData[index].videoLength = videoLength;
-                            });
+$(document).ready(function () {
+    const $videoTable = $("#videoTable");
+    const $searchForm = $("#searchForm");
+    const $searchInput = $("#searchInput");
+    const $dateFrom = $("#dateFrom");
+    const $dateTo = $("#dateTo");
 
-                            $('#playlist-table').DataTable({
-                                data: playlistData,
-                                columns: [
-                                    { data: 'title', orderable: false },
-                                    { data: 'uploadDate' },
-                                    { data: 'videoLength' },
-                                    { data: 'likes' },
-                                    { data: 'comments' }
-                                ]
-                            });
-                        },
-                        error: function(xhr, status, error) {
-                            console.log('Error: ' + error);
-                        }
-                    });
-                },
-                error: function(xhr, status, error) {
-                    console.log('Error: ' + error);
-                }
-            });
+    const table = $videoTable.DataTable({
+        lengthMenu: [10, 50, 100, 1000],
+    });
+
+    $searchForm.submit(function (event) {
+        event.preventDefault();
+
+        const searchQuery = $searchInput.val();
+        const dateFrom = $dateFrom.val();
+        let dateTo = $dateTo.val();
+
+        const apiParams = {
+            part: "snippet",
+            q: searchQuery,
+            key: "AIzaSyDjieCRye5T3oMUGna3uE9fqZ6XQMemtXU",
+            maxResults: 10000,
+        };
+
+        if (dateFrom) {
+            apiParams.publishedAfter = dateFrom + "T00:00:00Z";
         }
+
+        if (!dateTo) {
+            dateTo = new Date().toISOString().split("T")[0];
+        }
+        apiParams.publishedBefore = dateTo + "T23:59:59Z";
+
+        $.get(
+            "https://www.googleapis.com/youtube/v3/search",
+            apiParams,
+            function (data) {
+                table.clear().draw();
+
+                data.items.forEach((item) => {
+                    const videoId = item.id.videoId;
+                    const title = item.snippet.title;
+                    const uploadDate = item.snippet.publishedAt;
+
+                    const videoLink = `<a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">${title}</a>`;
+
+                    $.get(
+                        "https://www.googleapis.com/youtube/v3/videos",
+                        {
+                            part: "contentDetails,statistics",
+                            id: videoId,
+                            key: "AIzaSyDjieCRye5T3oMUGna3uE9fqZ6XQMemtXU",
+                        },
+                        function (videoData) {
+                            const duration = parseVideoDuration(
+                                videoData.items[0].contentDetails.duration
+                            );
+                            const likesCount =
+                                videoData.items[0].statistics.likeCount;
+                            const viewsCount =
+                                videoData.items[0].statistics.viewCount;
+
+                            table.row
+                                .add([
+                                    videoLink,
+                                    duration,
+                                    uploadDate,
+                                    likesCount,
+                                    viewsCount,
+                                ])
+                                .draw();
+
+                            const videoRating =
+                                videoData.items[0].statistics.averageRating;
+                            const rowIndex = table
+                                .column(0)
+                                .data()
+                                .indexOf(videoLink);
+                            if (rowIndex >= 0) {
+                                table.cell(rowIndex, 4).data(videoRating);
+                                table.draw();
+                            }
+                        }
+                    );
+                });
+            }
+        );
     });
 });
 
-function extractPlaylistId(url) {
-    var playlistId = '';
-    if (url.includes('list=')) {
-        var regex = /[?&]list=([^&#]*)/i;
-        var match = regex.exec(url);
-        if (match && match[1]) {
-            playlistId = match[1];
+// function parseVideoDuration(duration) {
+//     const matches = duration.match(/[0-9]+[HMS]/g);
+//     let seconds = 0;
+
+//     matches.forEach((part) => {
+//         const unit = part.charAt(part.length - 1);
+//         const value = parseInt(part.slice(0, -1));
+
+//         if (unit === "H") {
+//             seconds += value * 3600;
+//         } else if (unit === "M") {
+//             seconds += value * 60;
+//         } else if (unit === "S") {
+//             seconds += value;
+//         }
+//     });
+
+//     const hours = Math.floor(seconds / 3600);
+//     const minutes = Math.floor((seconds % 3600) / 60);
+//     let formattedDuration = "";
+
+//     if (hours > 0) {
+//         formattedDuration += hours + "h ";
+//     }
+//     if (minutes > 0) {
+//         formattedDuration += minutes + "m ";
+//     }
+//     formattedDuration += (seconds % 60) + "s";
+
+//     return formattedDuration;
+// }
+function parseVideoDuration(duration) {
+    const matches = duration.match(/[0-9]+[HMS]/g);
+    let hours = 0;
+
+    matches.forEach((part) => {
+        const unit = part.charAt(part.length - 1);
+        const value = parseInt(part.slice(0, -1));
+
+        if (unit === "H") {
+            hours += value;
+        } else if (unit === "M") {
+            hours += value / 60;
+        } else if (unit === "S") {
+            hours += value / 3600;
         }
-    } else if (url.includes('/playlist?list=')) {
-        var parts = url.split('/playlist?list=');
-        if (parts.length > 1) {
-            playlistId = parts[1].split('&')[0];
-        }
-    } else if (url.includes('/watch?')) {
-        var parts = url.split('/watch?');
-        if (parts.length > 1) {
-            var queryParams = new URLSearchParams(parts[1]);
-            playlistId = queryParams.get('list');
-        }
-    }
-    return playlistId;
-}
+    });
 
-function parseISO8601Duration(duration) {
-    var match = duration.match(/P(\d+Y)?(\d+W)?(\d+D)?T(\d+H)?(\d+M)?(\d+S)?/);
-
-    var years = parseInt(match[1]) || 0;
-    var weeks = parseInt(match[2]) || 0;
-    var days = parseInt(match[3]) || 0;
-    var hours = parseInt(match[4]) || 0;
-    var minutes = parseInt(match[5]) || 0;
-    var seconds = parseInt(match[6]) || 0;
-
-    var totalMinutes = years * 365 * 24 * 60 +
-        weeks * 7 * 24 * 60 +
-        days * 24 * 60 +
-        hours * 60 +
-        minutes +
-        Math.round(seconds / 60);
-
-    return Math.floor(totalMinutes / 60) + 'h ' + (totalMinutes % 60) + 'm';
+    return hours.toFixed(2) + "h";
 }
