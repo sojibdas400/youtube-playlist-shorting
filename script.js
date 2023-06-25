6bf378c53801e9f64964be961037c31397751e0e
sojib600 <sojibdas400@gmail.com>
2023-06-25 21:49:44 +0600

    api input field added

--- script.js
+++ script.js
@@ -1,86 +1,121 @@
 $(document).ready(function () {
-    const $videoTable = $("#videoTable");
-    const $searchForm = $("#searchForm");
-    const $searchInput = $("#searchInput");
-    const $dateFrom = $("#dateFrom");
-    const $dateTo = $("#dateTo");
+    // Initialize DataTable
+    var table = $("#videoTable").DataTable();
 
-    const table = $videoTable.DataTable({
-        lengthMenu: [10, 50, 100, 1000],
-    });
-
-    $searchForm.submit(function (event) {
+    // Event handler for the form submission
+    $("#searchForm").submit(function (event) {
         event.preventDefault();
 
-        const searchQuery = $searchInput.val();
-        const dateFrom = $dateFrom.val();
-        let dateTo = $dateTo.val();
+        // Get user input
+        var searchQuery = $("#searchInput").val();
+        var dateFrom = $("#dateFrom").val();
+        var dateTo = $("#dateTo").val();
+        var apiKey = $("#apiKey").val();
 
-        const apiParams = {
+        if (!apiKey) {
+            apiKey = "AIzaSyCE-3a0xnoM8lVjmojr8TAMpaEl94mEPag";
+        }
+
+        // Build the API request parameters
+        var apiParams = {
             part: "snippet",
             q: searchQuery,
-            key: "AIzaSyDjieCRye5T3oMUGna3uE9fqZ6XQMemtXU",
-            maxResults: 10000,
+            key: apiKey,
+            maxResults: 1000, // Change the number to the desired maximum results
         };
 
         if (dateFrom) {
             apiParams.publishedAfter = dateFrom + "T00:00:00Z";
         }
 
+        // Set dateTo to today's date if not provided
         if (!dateTo) {
-            dateTo = new Date().toISOString().split("T")[0];
+            var today = new Date();
+            dateTo = today.toISOString().split("T")[0];
         }
         apiParams.publishedBefore = dateTo + "T23:59:59Z";
 
+        // Make API request to search for videos
         $.get(
             "https://www.googleapis.com/youtube/v3/search",
             apiParams,
             function (data) {
+                // Clear existing table data
                 table.clear().draw();
 
-                data.items.forEach((item) => {
-                    const videoId = item.id.videoId;
-                    const title = item.snippet.title;
-                    const uploadDate = item.snippet.publishedAt;
+                // Process search results
+                data.items.forEach(function (item) {
+                    var videoId = item.id.videoId;
+                    var title = item.snippet.title;
+                    var uploadDate = item.snippet.publishedAt;
 
-                    const videoLink = `<a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">${title}</a>`;
+                    // Create a link to the video
+                    var videoLink =
+                        '<a href="https://www.youtube.com/watch?v=' +
+                        videoId +
+                        '" target="_blank">' +
+                        title +
+                        "</a>";
 
+                    // Make API request to retrieve video details
                     $.get(
                         "https://www.googleapis.com/youtube/v3/videos",
                         {
                             part: "contentDetails,statistics",
                             id: videoId,
-                            key: "AIzaSyDjieCRye5T3oMUGna3uE9fqZ6XQMemtXU",
+                            key: apiKey,
                         },
                         function (videoData) {
-                            const duration = parseVideoDuration(
-                                videoData.items[0].contentDetails.duration
-                            );
-                            const likesCount =
+                            console.log(videoData);
+                            // Extract video duration
+                            var duration =
+                                videoData.items[0].contentDetails.duration;
+                            var videoLength = parseVideoDuration(duration);
+
+                            // Extract video likes count
+                            var likesCount =
                                 videoData.items[0].statistics.likeCount;
-                            const viewsCount =
+                            var viewssCount =
                                 videoData.items[0].statistics.viewCount;
 
+                            // Create a row for each video
                             table.row
                                 .add([
                                     videoLink,
-                                    duration,
+                                    videoLength,
                                     uploadDate,
                                     likesCount,
-                                    viewsCount,
+                                    viewssCount,
                                 ])
                                 .draw();
 
-                            const videoRating =
-                                videoData.items[0].statistics.averageRating;
-                            const rowIndex = table
-                                .column(0)
-                                .data()
-                                .indexOf(videoLink);
-                            if (rowIndex >= 0) {
-                                table.cell(rowIndex, 4).data(videoRating);
-                                table.draw();
-                            }
+                            // Make API request to retrieve video statistics for rating
+                            $.get(
+                                "https://www.googleapis.com/youtube/v3/videos",
+                                {
+                                    part: "statistics",
+                                    id: videoId,
+                                    key: apiKey,
+                                },
+                                function (statData) {
+                                    // Extract video rating
+                                    var videoRating =
+                                        statData.items[0].statistics
+                                            .averageRating;
+
+                                    // Update the rating column in the DataTable
+                                    var rowIndex = table
+                                        .column(0)
+                                        .data()
+                                        .indexOf(videoLink);
+                                    if (rowIndex >= 0) {
+                                        table
+                                            .cell(rowIndex, 4)
+                                            .data(videoRating);
+                                        table.draw();
+                                    }
+                                }
+                            );
                         }
                     );
                 });
@@ -89,37 +124,7 @@ $(document).ready(function () {
     });
 });
 
-// function parseVideoDuration(duration) {
-//     const matches = duration.match(/[0-9]+[HMS]/g);
-//     let seconds = 0;
-
-//     matches.forEach((part) => {
-//         const unit = part.charAt(part.length - 1);
-//         const value = parseInt(part.slice(0, -1));
-
-//         if (unit === "H") {
-//             seconds += value * 3600;
-//         } else if (unit === "M") {
-//             seconds += value * 60;
-//         } else if (unit === "S") {
-//             seconds += value;
-//         }
-//     });
-
-//     const hours = Math.floor(seconds / 3600);
-//     const minutes = Math.floor((seconds % 3600) / 60);
-//     let formattedDuration = "";
-
-//     if (hours > 0) {
-//         formattedDuration += hours + "h ";
-//     }
-//     if (minutes > 0) {
-//         formattedDuration += minutes + "m ";
-//     }
-//     formattedDuration += (seconds % 60) + "s";
-
-//     return formattedDuration;
-// }
+// Function to parse the video duration from ISO 8601 format
 function parseVideoDuration(duration) {
     const matches = duration.match(/[0-9]+[HMS]/g);
     let hours = 0;
